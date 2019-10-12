b265d149f7e9b24679bebc90f624ba198d823dd9
julcarp <julcarp@umich.edu>
2019-10-12 14:45:56 -0400

    yeet

--- insertImage.js
+++ insertImage.js
@@ -1,24 +1,28 @@
 var imgURL = chrome.extension.getURL('images/judging.png');
 var div = document.createElement("DIV");
 div.id = "judger";
+div.class = "moveFront"
 var img = document.createElement("IMG");
 img.src = imgURL;
+img.style.position = 'fixed';
+img.style.bottom = '0';
+img.style.left = '0';
 div.appendChild(img);
 document.body.appendChild(div);
 
-    // window.onload = function () {
-    //         // your code 
-    //         document.getElementById("toggleButton").onclick = function () {
-    //             toggleOn()
-    //         };
-        
-    //         function toggleOn() {
-    //             var imgURL = chrome.extension.getURL('images/judging.png');
-    //             var div = document.createElement("DIV");
-    //             div.id = "judger";
-    //             var img = document.createElement("IMG");
-    //             img.src = imgURL;
-    //             div.appendChild(img);
-    //             document.body.appendChild(div);
-    //         }
-    //     };
+// window.onload = function () {
+//         // your code 
+//         document.getElementById("toggleButton").onclick = function () {
+//             toggleOn()
+//         };
+
+//         function toggleOn() {
+//             var imgURL = chrome.extension.getURL('images/judging.png');
+//             var div = document.createElement("DIV");
+//             div.id = "judger";
+//             var img = document.createElement("IMG");
+//             img.src = imgURL;
+//             div.appendChild(img);
+//             document.body.appendChild(div);
+//         }
+//     };
