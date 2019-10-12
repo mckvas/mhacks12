4407dabdd04525207794f8c767c530e864a6c37d
mckvas <mckvas@umich.edu>
2019-10-12 10:22:18 -0400

    changed names in js file -Matt

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-var imgURL = chrome.extension.getURL('pic.jpg');
+var imgURL = chrome.extension.getURL('images/judging.jpg');
 var div = document.createElement("DIV");
-div.id = "someName";
+div.id = "judger";
 var img = document.createElement("IMG");
 img.src = imgURL;
 div.appendChild(img);
