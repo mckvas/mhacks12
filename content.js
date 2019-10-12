48f056b4fc8578f5c415a834b7998079d33593a3
mckvas <mckvas@umich.edu>
2019-10-12 10:35:21 -0400

    created function to run when toggle on in js -Matt

--- content.js
+++ content.js
@@ -3,6 +3,9 @@ var div = document.createElement("DIV");
 div.id = "judger";
 var img = document.createElement("IMG");
 img.src = imgURL;
-div.appendChild(img);
-document.body.appendChild(div);
+
+function toggleOn() {
+    div.appendChild(img);
+    document.body.appendChild(div);
+}
  
